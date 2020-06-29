ab8bb548e3762072394277d47d3550eb786275a1
Boyu Ruan <b.ruan@uq.edu.au>
2020-06-29 11:16:02 +1000

    update multiple strategies

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 #include <cmath>
 #include <unordered_map>
 #include <unordered_set>
+#include <chrono>
 
 typedef unsigned int ui;
 
@@ -19,7 +20,7 @@ using namespace std;
 enum strategy {
     huge,
     uniform,
-    toss
+    peak
 };
 
 struct four_tuple {
@@ -52,19 +53,25 @@ namespace std {
 }
 
 struct Param {
-    ui b;
-    double avg_k;
+    ui n1;
+    ui n2;
+    ui k;
+    ui large;
+    double prob;
     strategy s;
 };
 
 struct Param parseArgs(int argc, char **argv) {
     Param rtn;
-    ui b = 0;
+    ui n1 = 0;
+    ui n2 = 0;
+    ui large = 2;
     char *arg;
     char para[15] = "";
     int i = 1;
     int cnt = 1;
-    double avg_k = 0.0;
+    double prob = 0.0;
+    ui k = 10;
     strategy s = strategy::uniform;
     stringstream ss;
     while (cnt < argc) {
@@ -76,92 +83,173 @@ struct Param parseArgs(int argc, char **argv) {
         getNextWord(arg + i + 1, para);
         cout << para << '\t';
         arg = argv[cnt++];
-        if (strcmp(para, "b") == 0) {
+        if (strcmp(para, "upper") == 0) {
             ss << arg;
-            ss >> b;
+            ss >> n1;
             ss.clear();
-            cout << b << endl;
+            cout << n1 << endl;
+        } else if (strcmp(para, "lower") == 0) {
+            ss << arg;
+            ss >> n2;
+            ss.clear();
+            cout << n2 << endl;
         } else if (strcmp(para, "k") == 0) {
-            ss << fixed << setprecision(1) << arg;
-            ss >> avg_k;
+            ss << arg;
+            ss >> k;
             ss.clear();
-            cout << avg_k << endl;
+            cout << k << endl;
+        } else if (strcmp(para, "prob") == 0) {
+            ss << fixed << setprecision(2) << arg;
+            ss >> prob;
+            ss.clear();
+            cout << prob << endl;
+        } else if (strcmp(para, "large") == 0) {
+            ss << arg;
+            ss >> large;
+            cout << large << endl;
         } else if (strcmp(para, "strategy") == 0) {
             if (strcmp(arg, "uniform") == 0) {
                 s = strategy::uniform;
+                cout << "uniform" << endl;
             } else if (strcmp(arg, "huge") == 0) {
                 s = strategy::huge;
-            } else if (strcmp(arg, "toss") == 0) {
-                s = strategy::toss;
+                cout << "huge" << endl;
+            } else if (strcmp(arg, "peak") == 0) {
+                s = strategy::peak;
+                cout << "peak" << endl;
             }
         } 
                 
     }
-    rtn.b = b;
-    rtn.avg_k = avg_k;
+    rtn.n1 = n1;
+    rtn.n2 = n2;
+    rtn.k = k;
+    rtn.prob = prob;
+    rtn.large = large;
     rtn.s = s;
     return rtn;
 }
 
 int main(int argc, char **argv) {
+    chrono::high_resolution_clock::time_point beginning = chrono::high_resolution_clock::now();
     struct Param paras = parseArgs(argc, argv); 
-    string _k, _b;
-    stringstream ss;
-    ss << fixed << setprecision(1) << paras.avg_k;
-    ss >> _k;
-    ss.clear();
-    ss << paras.b;
-    ss >> _b;
-    ss.clear();
-    ui m, n1, n2;
-    m = 0;
-    n1 = 0;
-    n2 = 0;
+    ui m = 0;
     // unordered_set<four_tuple> butterflies; 
     // unordered_set<ui> edges;
-    
-    if (paras.s == strategy::uniform) {
-        string filename = "uniform_" + _b + "_" + _k + ".bloom";
-    // cout << filename << endl;
+    vector<pair<ui, ui> > edges;
+    stringstream ss;
+    ss << fixed;
+    ss.precision(2);
+    ss << paras.prob;
+    string pr;
+    ss >> pr;
+    chrono::high_resolution_clock::duration d = chrono::high_resolution_clock::now() - beginning;
+    unsigned seed = d.count();
+    if (paras.s == strategy::uniform) { 
+        string filename = "uniform_" + to_string(paras.n1) + "_" + to_string(paras.n2) + "_" + pr;
         ofstream fout;
-        ui k = (ui) paras.avg_k;
-        fout.open(filename.c_str(), ios::out | ios::binary);
-        fout.write((char *) &m, sizeof(ui));
-        fout.write((char *) &n1, sizeof(ui));
-        fout.write((char *) &n2, sizeof(ui));
-        ui i = 1;
-        while (i <= paras.b) {
-            if (m == 0) {
-                fout.write((char *) &i, sizeof(ui));
-                fout.write((char *) &k, sizeof(ui));
-                for (ui j = 1; j <= 2 * k; j++) {
-                    fout.write((char *) &j, sizeof(ui));
-                    // edges.insert(j);
-                    m++; 
+        default_random_engine generator;
+        generator.seed(seed);
+        uniform_real_distribution<double> distribution(0.0, 1.0);
+        fout.open(filename.c_str(), ios::out);
+        for (ui i = 1; i <= paras.n1; i++) {
+            for (ui j = 1; j <= paras.n2; j++) {
+                double p = distribution(generator);
+                if (p <= paras.prob) {
+                    edges.push_back(make_pair(i, j));
+                    m++;
+                } else {
+                    continue;
                 }
-                i++;
-            } else {
-                ui t = m / k;
-                for (ui x = 0; x < t; x++) {
-                    fout.write((char *) &i, sizeof(ui));
-                    fout.write((char *) &k, sizeof(ui));
-                    for (ui j = x * k + 1; j <= (x + 1) * k; j++) {
-                        fout.write((char *) &j, sizeof(ui));
-                        m++;
-                    }
-                    for (ui j = t * k + 1; j <= (t + 1) * k; j++) {
-                        fout.write((char *) &j, sizeof(ui));
-                        m++;
-                    }
-                    i++;
-                }
-            } 
+            }
         }
-        fout.seekp(0);
-        fout.write((char *) &m, sizeof(ui));
+        fout << "% bip" << endl;
+        fout << "% " << m << " " << paras.n1 << " " << paras.n2 << endl;
+        for (ui i = 0; i < m; i++) {
+            fout << edges[i].first << " " << edges[i].second << endl;
+        }
+        fout.close();
 
     } else if (paras.s == strategy::huge) {
-
+        string filename = "huge_" + to_string(paras.n1) + "_" + to_string(paras.n2) + "_" + pr;
+        ofstream fout;
+        default_random_engine generator;
+        generator.seed(seed);
+        uniform_real_distribution<double> distribution(0.0, 1.0);
+        fout.open(filename.c_str(), ios::out);
+        for (ui i = 1; i <= paras.k; i++) {
+            edges.push_back(make_pair(1, i));
+            edges.push_back(make_pair(2, i));
+            m += 2;
+        }
+        for (ui i = paras.k + 1; i <= paras.n2; i++) {
+            double p = distribution(generator);
+            if (p <= paras.prob) {
+                edges.push_back(make_pair(1, i));
+                m++;
+            }
+            p = distribution(generator);
+            if (p <= paras.prob) {
+                edges.push_back(make_pair(2, i));
+                m++;
+            }
+        }
+        for (ui i = 3; i <= paras.n1; i++) {
+            for (ui j = 1; j <= paras.n2; j++) {
+                double p = distribution(generator);
+                if (p <= paras.prob) {
+                    edges.push_back(make_pair(i, j));
+                    m++;
+                } else {
+                    continue;
+                }
+            }
+        }
+        fout << "% bip" << endl;
+        fout << "% " << m << " " << paras.n1 << " " << paras.n2 << endl;
+        for (ui i = 0; i < m; i++) {
+            fout << edges[i].first << " " << edges[i].second << endl;
+        }
+        fout.close();
+    } else if (paras.s == strategy::peak) {
+        string filename = "peak_" + to_string(paras.n1) + "_" + to_string(paras.n2) + "_" + to_string(paras.large) + "_" + pr;
+        ofstream fout;
+        default_random_engine generator;
+        generator.seed(seed);
+        uniform_real_distribution<double> distribution(0.0, 1.0);
+        fout.open(filename.c_str(), ios::out);
+        for (ui i = 1; i <= paras.large; i++) {
+            for (ui j = 1; j <= paras.k; j++) {
+                edges.push_back(make_pair(i, j));
+                m++;
+            }
+            for (ui j = paras.k + 1; j <= paras.n2; j++) {
+                double p = distribution(generator);
+                if (p <= paras.prob) {
+                    edges.push_back(make_pair(i, j));
+                    m++;
+                } else {
+                    continue;
+                }
+            }
+        }
+        for (ui i = paras.large + 1; i <= paras.n1; i++) {
+            for (ui j = 1; j <= paras.n2; j++) {
+                double p = distribution(generator);
+                if (p <= paras.prob) {
+                    edges.push_back(make_pair(i, j));
+                    m++;
+                } else {
+                    continue;
+                }
+            }
+        }
+        fout << "% bip" << endl;
+        fout << "% " << m << " " << paras.n1 << " " << paras.n2 << endl;
+        for (ui i = 0; i < m; i++) {
+            fout << edges[i].first << " " << edges[i].second << endl;
+        }
+        fout.close();
     }
     
     return 0;
